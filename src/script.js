dbe46e8bcc89501819405ea5f8ea2a9f5b922333
Sindiswa <78302541+Nebularay@users.noreply.github.com>
2021-03-27 17:14:30 +0100

    Deleted duplicate code for forecast

--- src/script.js
+++ src/script.js
@@ -89,88 +89,8 @@ function showForecast(response) {
   `;  
 
     
-  
+}
 
-  // for (let index = 0; index < 6; index = ++) {
-  //   forecast = response.data.list[index];
-  //   forecastElement.innerHTML += `
-  //   <div class="col">
-  //       <div class="card-two" style="width: 6rem;">
-  //         <p>
-  //           ${formatHours(forecast.dt *1000)}
-  //         </p>
-  //         <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png">
-  //         <div class="card-body-two">
-  //         </div>
-  //       </div>
-  //       <div class="card-details-two" style="width: 6rem;">
-  //         <ul class="list-group list-group-flush">
-  //         <li class="sec-temperature">${Math.round(forecast.main.temp_max)}°C / ${Math.round(forecast.main.temp_min)}°C </li>
-  //         </ul>
-  //       </div>
-  //   </div>
-  // `;  
-
-  }
-
-  
-
-  // forecast = response.data.list[2];
-  // forecastElement.innerHTML += `
-  //   <div class="col">
-  //       <div class="card-two" style="width: 6rem;">
-  //         <p>
-  //           ${formatHours(forecast.dt *1000)}
-  //         </p>
-  //         <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png">
-  //         <div class="card-body-two">
-  //         </div>
-  //       </div>
-  //       <div class="card-details-two" style="width: 6rem;">
-  //         <ul class="list-group list-group-flush">
-  //         <li class="sec-temperature">${Math.round(forecast.main.temp_max)}°C / ${Math.round(forecast.main.temp_min)}°C </li>
-  //         </ul>
-  //       </div>
-  //   </div>
-  // `;  
-
-  // forecast = response.data.list[3];
-  // forecastElement.innerHTML += `
-  //   <div class="col">
-  //       <div class="card-two" style="width: 6rem;">
-  //         <p>
-  //           ${formatHours(forecast.dt *1000)}
-  //         </p>
-  //         <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png">
-  //         <div class="card-body-two">
-  //         </div>
-  //       </div>
-  //       <div class="card-details-two" style="width: 6rem;">
-  //         <ul class="list-group list-group-flush">
-  //         <li class="sec-temperature">${Math.round(forecast.main.temp_max)}°C / ${Math.round(forecast.main.temp_min)}°C </li>
-  //         </ul>
-  //       </div>
-  //   </div>
-  // `;  
-
-  // forecast = response.data.list[4];
-  // forecastElement.innerHTML += `
-  //   <div class="col">
-  //       <div class="card-two" style="width: 6rem;">
-  //         <p>
-  //           ${formatHours(forecast.dt *1000)}
-  //         </p>
-  //         <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png">
-  //         <div class="card-body-two">
-  //         </div>
-  //       </div>
-  //       <div class="card-details-two" style="width: 6rem;">
-  //         <ul class="list-group list-group-flush">
-  //         <li class="sec-temperature">${Math.round(forecast.main.temp_max)}°C / ${Math.round(forecast.main.temp_min)}°C </li>
-  //         </ul>
-  //       </div>
-  //   </div>
-  // `;  
 
 }
 
